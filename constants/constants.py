f4634b4fbfb4c3c86f3d43979eaf1b93cc451800
burakhanaksoy <burakhan.aksoy@tedu.edu.tr>
2021-04-09 08:20:12 +0300

    ready

---
+++ constants/constants.py
@@ -0,0 +1,6 @@
+blog_id = [11, 12, 33, 22, 55, 11, 2312, 23]
+user = ['Burakhan', 'RafetElMorad', 'anonymousUser', 'thenewboston']
+comments = ["wow! This post was what I just needed!",
+            "You earned my upvote pal!",
+            "Keep up with the good work",
+            "++"]
--- main.py
+++ main.py
@@ -1,12 +1,81 @@
 from fastapi import FastAPI
+# from typing import Optional
+from constants import constants
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return [{'data': {'student': {'name': 'burak', 'age': 29}}}]
+class BlogPost:
+    def __init__(self, post_id: int, user: str, comments: list):
+        self.post_id = post_id
+        self.user = user
+        self.comments = comments
 
-@app.get('/about')
-def about():
-    return {'data':{'About page'}}
+
+@app.get('/blog')
+def blog():
+    blog_list = []
+    for i in range(5):
+        data = BlogPost(constants.blog_id[i % 5], constants.user[i % 4], constants.comments[i % 4])
+        blog_list.append(data)
+    print(blog_list)
+    return {'data': {'blog_post': blog_list}}
+
+
+# @app.get('/blog')
+# def blog(limit: int = 10, published: bool = False):
+#     if published:
+#         return {'data': f'{limit} published blogs from the db'}
+#     else:
+#         return {'data': f'{limit} blogs from the db'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    # fetch comments with blog id = id
+    return {
+        'data': {'blog_post': [{'id': id, 'user': 'theBMan', 'likes': 123812,
+                                'comments': ['wow! This post was what I just needed!'
+                                    , 'You earned my upvote pal!'
+                                    , 'Keep up with the good work'
+                                    , '++']},
+                               {'id': id, 'user': 'theBMan', 'likes': 123812,
+                                'comments': ['wow! This post was what I just needed!'
+                                    , 'You earned my upvote pal!'
+                                    , 'Keep up with the good work'
+                                    , '++']}
+            ,
+                               {'id': id, 'user': 'theBMan', 'likes': 123812,
+                                'comments': ['wow! This post was what I just needed!'
+                                    , 'You earned my upvote pal!'
+                                    , 'Keep up with the good work'
+                                    , '++']}
+            ,
+                               {'id': id, 'user': 'theBMan', 'likes': 123812,
+                                'comments': ['wow! This post was what I just needed!'
+                                    , 'You earned my upvote pal!'
+                                    , 'Keep up with the good work'
+                                    , '++']}
+            ,
+                               {'id': id, 'user': 'theBMan', 'likes': 123812,
+                                'comments': ['wow! This post was what I just needed!'
+                                    , 'You earned my upvote pal!'
+                                    , 'Keep up with the good work'
+                                    , '++']}
+                               ]}}
+
+
+@app.post('/blog', status_code=201)
+def create_blog():
+    return {'data': 'Blog is created'}
